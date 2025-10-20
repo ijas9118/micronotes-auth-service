efaa963cef08be53fc9137b2fecbcabfe1fcf292
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2025-10-20 19:46:19 +0530

    🖥️ add prometheus monitoring

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import type { Application, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
+import client from "prom-client";
 
 import logger from "./configs/logger.js";
 import errorHandler from "./middleware/error-handler.js";
@@ -19,7 +20,28 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
 
+const register = new client.Registry();
+client.collectDefaultMetrics({ register });
+
+export const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total number of HTTP requests",
+  labelNames: ["method", "route", "status_code", "service"],
+});
+register.registerMetric(httpRequestCounter);
+
 app.use((req: Request, res: Response, next: NextFunction) => {
+  const routePath = req.route ? req.route.path : req.path;
+
+  res.on("finish", () => {
+    httpRequestCounter.inc({
+      method: req.method,
+      route: routePath,
+      status_code: res.statusCode,
+      service: "auth-service",
+    });
+  });
+
   logger.debug(`${req.method} ${req.url}`);
   next();
 });
@@ -33,6 +55,16 @@ app.get("/health", (_req, res) => {
 });
 
 app.use("/api/v1/auth", authRoutes);
+
+app.get("/metrics", async (_req: Request, res: Response) => {
+  try {
+    res.setHeader("Content-Type", register.contentType);
+    res.end(await register.metrics());
+  }
+  catch (err) {
+    res.status(500).end(err);
+  }
+});
 
 app.use((req: Request, res: Response) => {
   logger.warn(`Resource not found: ${req.method} ${req.url}`);
