b0737ca3f584f9615b1aa28562438f76af52b3ab
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2025-10-20 17:27:00 +0530

    update docker for dev

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import "reflect-metadata";
-import type { Application } from "express";
+import type { Application, NextFunction, Request, Response } from "express";
 
 import cors from "cors";
 import express from "express";
 import helmet from "helmet";
 
+import logger from "./configs/logger.js";
 import errorHandler from "./middleware/error-handler.js";
 import { limiter } from "./middleware/rate-limit.middleware.js";
 import authRoutes from "./routes/auth.routes.js";
@@ -18,14 +19,25 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(limiter);
 
-app.get("/health", (_req, res) =>
+app.use((req: Request, res: Response, next: NextFunction) => {
+  logger.debug(`${req.method} ${req.url}`);
+  next();
+});
+
+app.get("/health", (_req, res) => {
   res.json({
     status: "ok",
     message: "Auth service is healthy",
     timestamp: new Date().toISOString(),
-  }));
+  });
+});
 
 app.use("/api/v1/auth", authRoutes);
+
+app.use((req: Request, res: Response) => {
+  logger.warn(`Resource not found: ${req.method} ${req.url}`);
+  res.status(404).json({ message: "resource not found" });
+});
 
 app.use(errorHandler);
 
