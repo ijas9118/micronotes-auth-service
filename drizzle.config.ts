d1804ce26849d9520b4ced4075ec06312016e102
Ijas Ahammed <83322316+ijas9118@users.noreply.github.com>
2025-10-21 22:21:52 +0530

    fix env issue

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
+/* eslint-disable node/no-process-env */
 import { defineConfig } from "drizzle-kit";
-
-import env from "./src/configs/validate-env.ts";
 
 export default defineConfig({
   schema: "./src/db/schema.ts",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL || "postgresql://micronotes:cantremember@localhost:5432/micronotes_auth",
+    url: process.env.DATABASE_URL || "postgresql://micronotes:cantremember@localhost:5432/micronotes_auth",
   },
   verbose: true,
   strict: true,
